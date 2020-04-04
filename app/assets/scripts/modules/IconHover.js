397342be70ed48f55eba157bce676c3260609719
Peter Kertesz <kerteszpetr@gmail.com>
2020-04-04 17:32:54 +0100

    Designing and positioning icons in grid system, adding hover effect from JS, which has some problems at the moment.

--- app/assets/scripts/modules/IconHover.js
+++ app/assets/scripts/modules/IconHover.js
@@ -1,51 +1,28 @@
+import IconHTML from './content/Icons'; // Raw HTML for icons
+
 class IconHover {
 	constructor() {
 		this.IconUL = document.querySelector('.header__content__iconset');
 		this.injectHTML(this.IconUL);
+		this.LIs = document.querySelectorAll('li');
+		this.events();
+	}
+
+	events() {
+		// Listen for Hover event
+		this.LIs.forEach((icon) => {
+			icon.addEventListener('mouseover', (event) => {
+				this.toggleOnHover(icon);
+			});
+		});
+	}
+
+	toggleOnHover(icon) {
+		icon.classList.toggle('header__content__iconset--isLIHovered');
 	}
 
 	injectHTML(IconLocation) {
-		IconLocation.insertAdjacentHTML(
-			'beforeend',
-			`
-            <li>
-                <svg class="header__content__iconset__icon">
-                    <use xlink:href="assets/images/icons/sprite.svg#icon-document-file-html"></use>
-                </svg>
-                <span>HTML</span>
-            </li>
-            <li>
-                <svg class="header__content__iconset__icon">
-                    <use xlink:href="assets/images/icons/sprite.svg#icon-document-file-css"></use>
-                    <span>CSS</span>
-                </svg>
-            </li>
-            <li>
-                <svg class="header__content__iconset__icon">
-                    <use xlink:href="assets/images/icons/sprite.svg#icon-javascript"></use>
-                    <span>JavaScript</span>
-                </svg>
-            </li>
-            <li>
-                <svg class="header__content__iconset__icon">
-                    <use xlink:href="assets/images/icons/sprite.svg#icon-jquery"></use>
-                    <span>JQuery</span>
-                </svg>
-            </li>
-            <li>
-                <svg class="header__content__iconset__icon">
-                    <use xlink:href="assets/images/icons/sprite.svg#icon-node-dot-js"></use>
-                    <span>NodeJS</span>
-                </svg>
-            </li>
-            <li>
-                <svg class="header__content__iconset__icon">
-                    <use xlink:href="assets/images/icons/sprite.svg#icon-mongodb"></use>
-                    <span>MongoDB</span>
-                </svg>
-            </li>
-        `
-		);
+		IconLocation.insertAdjacentHTML('beforeend', IconHTML);
 	}
 }
 
---
+++ app/assets/scripts/modules/content/Icons.js
@@ -0,0 +1,40 @@
+const IconHTML = `
+<li>
+    <svg class="header__content__iconset__icon">
+        <use xlink:href="assets/images/icons/sprite.svg#icon-document-file-html"></use>
+    </svg>
+    <span>HTML</span>
+</li>
+<li>
+    <svg class="header__content__iconset__icon">
+        <use xlink:href="assets/images/icons/sprite.svg#icon-document-file-css"></use>
+        <span>CSS</span>
+    </svg>
+</li>
+<li>
+    <svg class="header__content__iconset__icon">
+        <use xlink:href="assets/images/icons/sprite.svg#icon-javascript"></use>
+        <span>JavaScript</span>
+    </svg>
+</li>
+<li>
+    <svg class="header__content__iconset__icon">
+        <use xlink:href="assets/images/icons/sprite.svg#icon-jquery"></use>
+        <span>JQuery</span>
+    </svg>
+</li>
+<li>
+    <svg class="header__content__iconset__icon">
+        <use xlink:href="assets/images/icons/sprite.svg#icon-node-dot-js"></use>
+        <span>NodeJS</span>
+    </svg>
+</li>
+<li>
+    <svg class="header__content__iconset__icon">
+        <use xlink:href="assets/images/icons/sprite.svg#icon-mongodb"></use>
+        <span>MongoDB</span>
+    </svg>
+</li>
+`;
+
+export default IconHTML;
