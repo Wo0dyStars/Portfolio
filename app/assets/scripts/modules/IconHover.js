22403e36e66f6b9178f2471527afe94f7d6edf4a
Peter Kertesz <kerteszpetr@gmail.com>
2020-04-04 17:52:25 +0100

    Resolved Hover effect, but only with pure CSS, no JS.

--- app/assets/scripts/modules/IconHover.js
+++ app/assets/scripts/modules/IconHover.js
@@ -4,21 +4,6 @@ class IconHover {
 	constructor() {
 		this.IconUL = document.querySelector('.header__content__iconset');
 		this.injectHTML(this.IconUL);
-		this.LIs = document.querySelectorAll('li');
-		this.events();
-	}
-
-	events() {
-		// Listen for Hover event
-		this.LIs.forEach((icon) => {
-			icon.addEventListener('mouseover', (event) => {
-				this.toggleOnHover(icon);
-			});
-		});
-	}
-
-	toggleOnHover(icon) {
-		icon.classList.toggle('header__content__iconset--isLIHovered');
 	}
 
 	injectHTML(IconLocation) {
