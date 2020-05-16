18d6343ddb7b8b7c2a2d7e763c52a6a96447d520
Peter Kertesz <kerteszpetr@gmail.com>
2020-05-16 19:24:58 +0100

    Revealing animations on section titles on scrolling.

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -2,11 +2,12 @@ import debounce from 'lodash/debounce';
 import throttle from 'lodash/throttle';
 
 class RevealOnScroll {
-	constructor(elements, CSSclass) {
+	constructor(elements, CSSclass, outerClass = 'none') {
 		this.items = elements;
 		this.browserHeight = window.innerHeight;
 		this.scrolling = throttle(this.calculateElements, 200).bind(this);
 		this.newClass = CSSclass;
+		this.outerClass = outerClass;
 		this.events();
 	}
 
@@ -21,16 +22,29 @@ class RevealOnScroll {
 		);
 	}
 
-	scrolledElement(element) {
+	scrolledElement(element, target = 'none') {
+		console.log('offset: ', element.offsetTop);
+		console.log('this.browserHeight: ', this.browserHeight);
+		console.log('window.scrollY: ', window.scrollY);
+
+		console.log('element: ', element);
 		if (window.scrollY + this.browserHeight > element.offsetTop) {
-			element.classList.add(this.newClass);
+			if (target == 'none') {
+				element.classList.add(this.newClass);
+			} else {
+				target.classList.add(this.newClass);
+			}
 		}
 	}
 
 	calculateElements() {
-		this.items.forEach((element) => {
-			this.scrolledElement(element);
-		});
+		if (this.outerClass == 'none') {
+			this.items.forEach((element) => {
+				this.scrolledElement(element);
+			});
+		} else {
+			this.scrolledElement(this.items, this.outerClass);
+		}
 	}
 }
 
