b63cad08600955f7d30b6fc301e3dc8f15bb7c76
Peter Kertesz <kerteszpetr@gmail.com>
2020-06-04 05:38:23 +0100

    Using img as a source for SVGs for docs to be available. Added docs folder for production.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,10 @@
-// Installed packages:
-//
-// ***********
-// For webpack initialization
-// webpack
-// webpack-cli
-//
-// ************
-// For webpack to understand CSS as well
-// css-loader = understands and bundles CSS files
-// style-loader = applies and uses CSS
-//
-// ************
-// For webpack to understand postCSS syntax
-// postcss-loader = loads post CSS with some options set up
-// postcss-simple-vars = allows for declaring variables
-// postcss-nested = allows for nesting css selectors
-// autoprefixer
-// postcss-import = not force browsers to download all CSS files
-// postcss-mixins =
-//
-// *************
-// normalize.css = For initializing browser configurations
-
-// This is part of Node library
+const currentTask = process.env.npm_lifecycle_event; // Select "dev" or "build"
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // Remove everything before saving new version
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Extract CSS out of JS file
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fse = require('fs-extra');
 
-// Plugins for post CSS to work as expected
 const postCSSPlugins = [
 	require('postcss-import'),
 	require('postcss-nested'),
@@ -34,29 +13,55 @@ const postCSSPlugins = [
 	require('postcss-mixins')
 ];
 
-module.exports = {
-	// Entry point of webpack server
+class RunAfterCompile {
+	apply(compiler) {
+		compiler.hooks.done.tap('Copy images', function() {
+			fse.copySync('./app/assets/images', './docs/assets/images');
+		});
+	}
+}
+
+let cssConfig = {
+	test: /\.css$/i,
+	use: [ 'css-loader?url=false', { loader: 'postcss-loader', options: { plugins: postCSSPlugins } } ]
+};
+
+let config = {
 	entry: './app/assets/scripts/app.js',
-	// saving bundled JS file
-	output: {
-		filename: 'bundled.js',
-		path: path.resolve(__dirname, 'app')
-	},
-	// Setting up this server for local environment
-	mode: 'development',
-	watch: true, // watches for changes
+	plugins: [],
 	module: {
-		// Make webpack understand CSS
-		rules: [
-			{
-				test: /\.css$/i, // Only filenames that end with .css
-				use: [
-					'style-loader',
-					'css-loader?url=false',
-					// this line is for post CSS
-					{ loader: 'postcss-loader', options: { plugins: postCSSPlugins } }
-				]
-			}
-		]
+		rules: [ cssConfig ]
 	}
 };
+
+if (currentTask == 'dev') {
+	cssConfig.use.unshift('style-loader');
+	config.output = {
+		filename: 'bundled.js',
+		path: path.resolve(__dirname, 'app')
+	};
+	config.watch = true;
+	config.mode = 'development';
+}
+
+if (currentTask == 'build') {
+	cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+	postCSSPlugins.push(require('cssnano'));
+	config.output = {
+		filename: '[name].[chunkhash].js',
+		chunkFilename: '[name].[chunkhash].js',
+		path: path.resolve(__dirname, 'docs')
+	};
+	config.mode = 'production';
+	config.optimization = {
+		splitChunks: { chunks: 'all' }
+	};
+	config.plugins.push(
+		new HtmlWebpackPlugin({ filename: 'index.html', template: './app/index.html' }),
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({ filename: 'styles.[chunkhash].css' }),
+		new RunAfterCompile()
+	);
+}
+
+module.exports = config;
